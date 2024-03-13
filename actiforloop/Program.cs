54c363e48b4b2dfb00895e944e521cd6235e8f37
Admin <Admin@TUAZON-FAMILY-P>
2024-03-13 18:37:30 +1300

    Add project files.

---
+++ actiforloop/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace taskTwo
+{
+    class Prgrm
+    {
+        static void Main(string[] args)
+        {
+            int i1;
+           
+
+            for (i1 = 1; i1 <= 100; i1++)
+            {
+
+                if (i1 % 4 == 0 || i1 % 6 == 0)
+                {
+                    Console.WriteLine("These numbers are divisible by 4 and 6: " + i1);
+                }
+            }
+
+        }
+    }
+}
